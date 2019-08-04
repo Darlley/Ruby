0c8d920d63b4345c213b72b0699f1cb76c8df20b
Darlley <37590954+Darlley@users.noreply.github.com>
2019-08-04 16:52:06 -0400

    Update ex01.rb

--- One-Bit-Code/aula3/ex01.rb
+++ One-Bit-Code/aula3/ex01.rb
@@ -1,29 +1,44 @@
-puts "====== ====== ====== M E N U ====== ====== ====== "
-puts "[1] ADIÇÃO; [2] SUBTRAÇÃO; [3] MULTIPLICAÇÃO; [4] DIVISÃO"
+escolha = 1
+while escolha > 0  
+  puts " "
 
-print "Qual operação você deseja efetuar? "
-escolha = gets.chomp.to_i
+  puts "====== ====== ====== M E N U ====== ====== ====== "
+  puts "[1] ADIÇÃO; [2] SUBTRAÇÃO; [3] MULTIPLICAÇÃO; [4] DIVISÃO; [0] SAIR"
 
-puts " "
+  print "Qual operação você deseja efetuar? "
+  escolha = gets.chomp.to_i
 
-print "Digite o primeiro valor: "
-n1 = gets. chomp.to_i
-print "Digite o segundo valor: "
-n2 = gets.chomp.to_i
+  puts " "
 
-puts " "
+  
 
-mensagem = "RESULTADO: " 
+  case escolha
+  when 1..4
+    puts " "
+    
+    print "Digite o primeiro valor: "
+    n1 = gets. chomp.to_i
+    print "Digite o segundo valor: "
+    n2 = gets.chomp.to_i
 
-case escolha 
-when 1
-  print "#{mensagem} #{n1} + #{n2} = #{n1+n2}"
-when 2
-  print "#{mensagem} #{n1} - #{n2} = #{n1-n2}"
-when 3
-  print "#{mensagem} #{n1} × #{n2} = #{n1*n2}"
-when 4
-  print "#{mensagem} #{n1} ÷ #{n2} = #{n1/n2}"
-else 
-  puts "Valor da operação inválido!"
+    puts " "
+    
+    mensagem = "RESULTADO: " 
+
+    case escolha
+    when 1
+      print "#{mensagem} #{n1} + #{n2} = #{n1+n2}"
+    when 2
+      print "#{mensagem} #{n1} - #{n2} = #{n1-n2}"
+    when 3
+      print "#{mensagem} #{n1} × #{n2} = #{n1*n2}"
+    else
+      print "#{mensagem} #{n1} ÷ #{n2} = #{n1/n2}"
+    end
+
+  when 0
+    puts "encerrando processo . . ."
+  else
+    puts "Valor da operação inválido!"
+  end
 end
