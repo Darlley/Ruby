f527903c343cce7c55b9c279079b7a1cbbd68403
Darlley <37590954+Darlley@users.noreply.github.com>
2019-08-04 11:44:42 -0400

    Create ex01.rb

---
+++ One-Bit-Code/aula2/ex01.rb
@@ -0,0 +1,32 @@
+var = 5
+var.class
+=> Integer
+
+var = 12.05
+var.class
+=> Float
+
+var = true
+var.class
+=> TrueClass
+
+
+var = false
+var.class
+=> FalseClass
+
+var = "Isto é uma String"
+var.class
+=> String
+
+var = ["indíce 0", 1, [2]] // Array com uma String("indíce 0"), um número inteiro (1), e  outro Array de uma posição ([2])
+var.class
+=> Array
+
+var = :Symbol_RepresentaAlgo
+var.class
+=> Symbol
+
+var = {Propriedade1: "Valor 1", Propriedade2: 2, Propriedade3: [3]}
+var.class
+=> Hash
