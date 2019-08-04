74eb7e16e00759abc3021109483d58285bd7bbf7
Darlley <37590954+Darlley@users.noreply.github.com>
2019-08-04 12:41:02 -0400

    Create ex03.rb

---
+++ One-Bit-Code/aula2/ex03.rb
@@ -0,0 +1,10 @@
+print "Informe o primeiro valor: "
+primeiroValor = gets.chomp.to_i
+print "Informe o segun do valor: "
+segundoValor = gets.chomp.to_i
+
+puts "A soma de #{primeiroValor} + #{segundoValor} = #{primeiroValor + segundoValor}"
+puts "A subtração de #{primeiroValor} - #{segundoValor} = #{primeiroValor - segundoValor}"
+puts "A multiplicação de #{primeiroValor} × #{segundoValor} = #{primeiroValor * segundoValor}"
+puts "A divisão de #{primeiroValor} ÷ #{segundoValor} = #{primeiroValor / segundoValor}"
+puts "O resto da divisão entre #{primeiroValor} / #{segundoValor} = #{primeiroValor % segundoValor}"
