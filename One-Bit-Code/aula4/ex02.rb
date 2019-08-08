6ca482072ff804a236e24cf90c3b94a03c00cc87
Darlley <37590954+Darlley@users.noreply.github.com>
2019-08-08 16:45:11 -0400

    ✖

---
+++ One-Bit-Code/aula4/ex02.rb
@@ -0,0 +1,24 @@
+print "informe o primeiro elemento: "
+elemento1 = gets.chomp
+print "informe o primeiro valor: "
+valor1 = gets.chomp
+
+print "informe o segundo elemento: "
+elemento1 = gets.chomp
+print "informe o segundo valor: "
+valor1 = gets.chomp
+
+print "informe o terceiro elemento: "
+elemento1 = gets.chomp
+print "informe o terceiro valor: "
+valor1 = gets.chomp
+
+objeto = {
+  #{elemento1}: #{valor1},
+  #{elemento2}: #{valor2},
+  #{elemento3}: #{valor3}
+}
+
+puts "Uma das chaves é #{objeto{1}} e o seu valor é #{valor1}"
+puts "Uma das chaves é #{objeto{1}} e o seu valor é #{valor2}"
+puts "Uma das chaves é #{objeto{1}} e o seu valor é #{valor3}"
